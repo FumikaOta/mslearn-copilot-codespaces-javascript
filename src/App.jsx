a810a42c4247a370639a633f1ab771fd5f6e3f0c
FumikaOta <fumika.ota@avanade.com>
2025-10-28 09:51:40 +0000

    Update siteProps with user information and add hover animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Fumika Ota",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "fumika@example.com",
+  gitHub: "fumika",
+  instagram: "fumika",
+  linkedIn: "fumika",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "fumika",
+  youTube: "fumika",
 };
 
 const primaryColor = "#4E567E";
